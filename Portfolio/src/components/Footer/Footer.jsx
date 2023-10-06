8405ace156a5abacf4b7f6b3e7b22915f192e4be
Daniel-Botache <daniel.botache@gmail.com>
2023-10-06 02:20:24 -0500

    se termina el estilo del footer

--- Portfolio/src/components/Footer/Footer.jsx
+++ Portfolio/src/components/Footer/Footer.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { FaCopyright } from "react-icons/fa";
 import styles from "./Footer.module.css";
+import MAIL from "../../assets/Icons/147563.svg";
+import CV from "../../assets/Icons/cv.svg";
 const Footer = () => {
   return (
     <footer className={styles.mainContainerFooter}>
-      <div className="footer-content">
-        <div className="footer-info">
-          <p>daniel.botache@gmail.com</p>
-          <p>
-            DB Dev todos los derechos reservados &copy;{" "}
-            {new Date().getFullYear()}
-            <FaCopyright />
-          </p>
-        </div>
+      <div className={styles.emailContainer}>
+        <img src={MAIL} alt="mail icon" className={styles.imgFooter} />
+        <p>daniel.botache@gmail.com</p>
       </div>
+      <div className={styles.emailContainer}>
+        <img src={CV} alt="cv icon" className={styles.imgFooter} />
+        <p>Mi CV</p>
+      </div>
+      <p>
+        <FaCopyright /> <span className={styles.spanFooter}>DB Dev </span>{" "}
+        {new Date().getFullYear()}
+      </p>
     </footer>
   );
 };
